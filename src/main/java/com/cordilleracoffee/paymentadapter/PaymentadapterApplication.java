af87231f8092d23e525056d2b8c55052c785d2b2
David Cediel <esdago18@gmail.com>
2025-04-30 22:42:19 +0200

    First commit, create project

---
+++ src/main/java/com/cordilleracoffee/paymentadapter/PaymentadapterApplication.java
@@ -0,0 +1,13 @@
+package com.cordilleracoffee.paymentadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentadapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentadapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cordilleracoffee/paymentadapter/PaymentadapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cordilleracoffee.paymentadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentadapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
