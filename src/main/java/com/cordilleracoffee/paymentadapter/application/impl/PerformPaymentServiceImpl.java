a9d1f3377c3afa59ba38423c99d6eaa93642ec37
David Cediel <esdago18@gmail.com>
2025-05-01 19:18:28 +0200

    Code happy path of perform payment

---
+++ src/main/java/com/cordilleracoffee/paymentadapter/application/impl/PerformPaymentServiceImpl.java
@@ -0,0 +1,37 @@
+package com.cordilleracoffee.paymentadapter.application.impl;
+
+
+import com.cordilleracoffee.paymentadapter.application.dto.PaymentGatewayRequest;
+import com.cordilleracoffee.paymentadapter.application.dto.PaymentGatewayResponse;
+import com.cordilleracoffee.paymentadapter.application.mappers.PaymentMapper;
+import com.cordilleracoffee.paymentadapter.infrastructure.dto.PaymentRequest;
+import com.cordilleracoffee.paymentadapter.infrastructure.dto.PaymentResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class PerformPaymentServiceImpl {
+
+    private final WebClient webClient;
+    private final PaymentMapper paymentMapper;
+
+    public Mono<PaymentResponse> performPayment(Mono<PaymentRequest> paymentRequest) {
+
+        return paymentRequest
+                .map(paymentMapper::toGatewayRequest)
+                .flatMap(this::callPaymentGateway)
+                .map(paymentMapper::toResponse);
+
+
+    }
+
+    private Mono<PaymentGatewayResponse> callPaymentGateway(PaymentGatewayRequest gatewayRequest) {
+        return webClient.post()
+                .bodyValue(gatewayRequest)
+                .retrieve()
+                .bodyToMono(PaymentGatewayResponse.class);
+    }
+}
